af4488801fd42600e771c2dc54f8a030901813a4
Cade Call <cadec@linuxjunk.org>
2019-11-08 14:39:53 -0700

    moved docker install to stage 2

---
+++ stage2/03-install-docker/01-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+install -m 644 files/docker.list	"${ROOTFS_DIR}/etc/apt/sources.list.d/docker.list"
+install -m 644 files/docker.gpg "${ROOTFS_DIR}/etc/docker.gpg"
+
+on_chroot << EOF
+cat /etc/docker/gpg | apt-key add -qq -
+
+apt-get update
+apt-get install -y -qq --no-install-recommends docker-ce
+usermod -aG docker pi
+EOF
